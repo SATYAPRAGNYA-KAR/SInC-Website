38618a6f94c2ad093fea8f98000c7ce6f01c82c1
SATYAPRAGNYA-KAR <satyakar2019@gmail.com>
2022-01-31 16:33:37 +0530

    API Removed, Favicon Added

--- src/Mentors_StartUpsYouFollow/Mentors_StartUpsYouFollowContent.js
+++ src/Mentors_StartUpsYouFollow/Mentors_StartUpsYouFollowContent.js
@@ -1,50 +1,24 @@
 import React from "react";
 import "./Mentors_StartUpsYouFollowContent.css";
 import Infocard from "./Mentors_StartUpsYouFollowInfocard";
-
-class Contact extends Component {
-  // Constructor
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://shivamjain123.pythonanywhere.com/api/startup-api")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
-      });
-  }
-  render() {
-    var { dataisLoaded, items } = this.state;
-
-    if (!dataisLoaded) {
-      console.log("Loading ...");
-    }
-
-    return (
-      <div className="mentorsstartupsyoufollowContent">
-        {items.map((item) => (
-          <Infocard
-            key={item.startup}
-            email={item.email}
-            startup_name={item.startup_name}
-            vision={item.vision}
-            type={item.type}
-            description={item.description}
-            short_description={item.short_description}
-          />
-        ))}
-      </div>
-    );
-  }
+function Content() {
+  return (
+    <div className="mentorsstartupsyoufollowContent">
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <Infocard />
+      <div> </div>
+    </div>
+  );
 }
+
 export default Content;
