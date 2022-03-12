e489f4dea803e772d4122e52f3cd489ad3292299
Nitesh Saini <64362683+nitesh047@users.noreply.github.com>
2022-03-12 22:19:03 +0530

    Update ContactUsApp.js

--- src/ContactUs/ContactUsApp.js
+++ src/ContactUs/ContactUsApp.js
@@ -6,6 +6,7 @@ import "./ContactUsApp.css";
 import Form from "./components/ContactUsForm";
 import Header from "./ContactUsHeader";
 import Footer from "./ContactUsFooter";
+import Topbar from './Navbar';
 // import bphone from "./images/backgroundphone.png";
 import facebook from "./images/facebook.png";
 import insta from "./images/insta.png";
@@ -24,13 +25,13 @@ function ContactUsApp() {
       //   backgroundRepeat: 'no-repeat'
       // }}
     >
-      <div className="contactus_header">
-        <Header />
-      </div>
+      {/* <div className="contactus_header"> */}
+       <Topbar/>
+      {/* </div> */}
 
-      <div className="contact-us-heading">Contact Us</div>
+     <center> <div className="contact-us-heading">Contact Us</div></center>
       <div className="contact-us-box">
-        <div className="contactuscontent-left">
+        {/* <div className="contactuscontent-left">
           <div className="contactusleft-heading">Address</div>
           <div className="contactusleft-body">
             Student Incubation Cell, Indian Institute of Technology-Delhi, Hauz
@@ -67,6 +68,47 @@ function ContactUsApp() {
 
         <div className="contactusright-box">
           <Form />
+        </div> */}
+        <div class="container">
+          <div className="row">
+            <div className="col-md-6">
+            <div className="contactusleft-heading">Address</div>
+          <div className="contactusleft-body">
+            Student Incubation Cell, Indian Institute of Technology-Delhi, Hauz
+            Khas, New Delhi-110016
+          </div>
+
+          <div className="contactusleft-heading">Let's Talk</div>
+          <div className="contactusleft-body">+234 081-1236-4568</div>
+
+          <div className="contactusleft-heading">General Support</div>
+          <div className="contactusleft-body">sinciitd@gmail.com</div>
+
+          <div className="contactussocial-media">
+            <a href="https://www.facebook.com/sinciitd">
+              <img src={facebook} alt="facebook icon" />
+            </a>
+            <a href="https://www.instagram.com/sinciitd/">
+              <img src={insta} alt="instagram icon" />
+            </a>
+            <a href="https://www.linkedin.com/company/sinciitd/mycompany/">
+              <img src={linkedin} alt="linkedIn icon" />
+            </a>
+            <a href="https://twitter.com/sinc_iitd">
+              <img src={twitter} alt="twitter icon" />
+            </a>
+          </div>
+
+          <img
+            className="contactussocialtree"
+            src={socialtree}
+            alt="A tree of all social media handles"
+          />
+            </div>
+            <div className="col-md-6">
+            <Form />
+              </div>
+          </div>
         </div>
       </div>
       <div className="contactus_footer">
