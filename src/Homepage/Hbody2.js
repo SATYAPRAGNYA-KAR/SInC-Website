37d63fb921856c95cecb21e64a814641d0ae0846
Nitesh Saini <64362683+nitesh047@users.noreply.github.com>
2022-04-16 23:21:30 +0530

    Added new Homepage

---
+++ src/Homepage/Hbody2.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Topbar from './Navbar'
+import './text.css'
+
+const Hbody2 = () => {
+  return (
+      <>
+      <Topbar/>
+      <div class="brasil" data-text="Student Incubation Cell">Student Incubation Cell</div> 
+      <center><h1 className='head'>IIT DELHI</h1></center>
+      <h1 className='head2'><i>“ CREATING JOB CREATORS ”</i></h1>
+      <h1 className='head3'><a style={{ backgroundColor: "#ffff00",fontSize:'20px'}} className='btn '><b>What We Do?</b></a></h1>
+      <div className='container-fluid bg-img'>
+      <div className='container-fluid nopadding' style={{height: "100%",
+   width: "100%",
+   background: "#000000cf"}}>
+       </div>
+
+      </div>
+      <div className='container'>
+         
+      </div>
+      </>
+   
+  )
+}
+
+export default Hbody2
