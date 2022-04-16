cabc56692b49aeb7ac782d9fecf003ee0a00aae2
Nitesh Saini <64362683+nitesh047@users.noreply.github.com>
2022-04-16 23:28:42 +0530

    Update Hbody2.js

--- src/Homepage/Hbody2.js
+++ src/Homepage/Hbody2.js
@@ -10,7 +10,7 @@ const Hbody2 = () => {
       <center><h1 className='head'>IIT DELHI</h1></center>
       <h1 className='head2'><i>“ CREATING JOB CREATORS ”</i></h1>
       <h1 className='head3'><a style={{ backgroundColor: "#ffff00",fontSize:'20px'}} className='btn '><b>What We Do?</b></a></h1>
-      <div className='container-fluid bg-img'>
+      <div className='container-fluid bg-img p-0'>
       <div className='container-fluid nopadding' style={{height: "100%",
    width: "100%",
    background: "#000000cf"}}>
